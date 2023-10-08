b9ee3b189b108b57faf35a61b867e644c805fb6f
Atilio <atilio.jesus@grupoboticario.com.br>
2023-10-08 16:40:02 -0300

    First structure frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tip Calculator</h1>
+      <h2>Enter the bill amount and tip percentage to calculate the total.</h2>
+      <p>Bill amount: </p>
+      <input/>
+      <p>Tip percentage:</p>
+      <input/>
+      <button>Calculate</button>
+      <h3>Total: </h3>
     </div>
   );
 }
