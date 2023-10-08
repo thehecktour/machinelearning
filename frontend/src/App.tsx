394c543a0d3766fc83d7370e63e66a4bdffe79b7
Atilio <atilio.jesus@grupoboticario.com.br>
2023-10-08 20:44:20 -0300

    Final stage of frontend and mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  
+  const [bill, setBill] = useState('');
+  const [tip, setTip] = useState('');
+  const [result, setResult] = useState('');
+
+  const calculate = () =>{
+    const finalBill = parseFloat(bill);
+    const finalTip = parseFloat(tip);
+    const finalResult = (finalBill*(finalTip/100));
+
+    if(!isNaN(finalBill) && !isNaN(finalTip)){
+      setResult(finalResult.toFixed(2));
+    }else{
+      setResult('0');
+    }
+  }
+
   return (
     <div className="App">
       <h1>Tip Calculator</h1>
       <h2>Enter the bill amount and tip percentage to calculate the total.</h2>
       <p>Bill amount: </p>
-      <input/>
+      <input
+        value={bill}
+        onChange={(finalValue)=> setBill(finalValue.target.value)}
+        placeholder='Whats your bill amount?'
+      />
       <p>Tip percentage:</p>
-      <input/>
-      <button>Calculate</button>
-      <h3>Total: </h3>
+      <input
+        value={tip}
+        onChange={(finalValue)=> setTip(finalValue.target.value)}
+        placeholder='Whats your tip percentage?'
+      />
+      <button onClick={calculate}>Calculate</button>
+      <h3>Total: {result}</h3>
     </div>
   );
 }
--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [bill, setBill] = useState('');
+  const [tip, setTip] = useState('');
+  const [result, setResult] = useState('');
+
+  const calculate = () =>{
+    const finalBill = parseFloat(bill);
+    const finalTip = parseFloat(tip);
+    const finalResult = (finalBill*(finalTip/100));
+
+    if(!isNaN(finalBill) && !isNaN(finalTip)){
+      setResult(finalResult.toFixed(2));
+    }else{
+      setResult('0');
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Tip Calculator</Text>
+      <Text style={styles.subtitulo}>Enter the bill and tip percentage.</Text>
+      <Text style={styles.perguntas}>Bill amount:</Text>
+      <TextInput
+        value={bill}
+        onChangeText={(value)=> setBill(value)}
+        keyboardType='numeric'
+        placeholder='Whats your bill amount?'
+      />
+      <Text style={styles.perguntas}>Write your tip percentage (%):</Text>
+      <TextInput
+        value={tip}
+        onChangeText={(value)=> setTip(value)}
+        keyboardType='numeric'
+        placeholder='Whats your tip percentage?'
+      />
+      <TouchableOpacity onPress={calculate}>
+        <Text style={styles.botao}>Calculate</Text>
+      </TouchableOpacity>
+      <Text style={styles.resultado}>Total: {result}</Text>
     </View>
   );
 }
@@ -13,8 +49,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5dc11b33',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    marginBottom: 10,
+  },
+  subtitulo:{
+    margin: 10,
+  },
+  perguntas:{
+    margin: 10,
+  },
+  botao: {
+    margin: 10,
+  },
+  resultado:{
+    margin: 10,
+  }
 });
