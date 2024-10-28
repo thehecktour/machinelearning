07b690b7a3cc18ef5381cceaa61f9c720cddb15e
Atilio <atilio.jesus@grupoboticario.com.br>
2024-10-27 21:43:39 -0300

    chore: 🚧 remove codigo nao utilizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
 import React, { useState } from 'react';
-import './App.css';
 
 function App() {
 
-  const [bill, setBill] = useState('');
-  const [tip, setTip] = useState('');
-  const [result, setResult] = useState('');
-
-  const calculate = () => {
-    const finalBill = parseFloat(bill);
-    const finalTip = parseFloat(tip);
-    const finalResult = (finalBill*(finalTip/100));
-
-    if(!isNaN(finalBill) && !isNaN(finalTip)){
-      setResult(finalResult.toFixed(2));
-    }else{
-      setResult('0');
-    }
-  }
-
+  
   return (
-    <div className='App'>
-      <h1>Tip Calculator</h1>
-      <h2>Whats your bill?</h2>
-      <input
-        value={bill}
-        onChange={(e)=> setBill(e.target.value)}
-        placeholder='Your bill vale'
-      />
-      <h2>Whats your tip?</h2>
-      <input
-        value={tip}
-        onChange={(e)=> setTip(e.target.value)}
-        placeholder='Your tip value'
-      />
-      <button onClick={calculate}>Calculate</button>
-      <h3>Your tip is ${result} dollars</h3>
+    <div>
+      <h1>Par ou Impar</h1>
+
     </div>
   );
 }
